b5762638ae119e5faecd00312326556b86d87639
Guilherme Barbosa <guilherme.barbosa@tatic.net>
2022-11-02 07:35:04 -0300

    feat: template method/command design pattern

---
+++ src/main/java/io/gdfbarbosa/patterns/behavioral/command/CommandClient.java
@@ -0,0 +1,8 @@
+package io.gdfbarbosa.patterns.behavioral.command;
+
+public class CommandClient {
+    public static void main(String[] args) {
+        Editor editor = new Editor();
+        editor.init();
+    }
+}
