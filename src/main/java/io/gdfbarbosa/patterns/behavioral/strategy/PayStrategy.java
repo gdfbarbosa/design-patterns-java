2afe2d1b34e0f20d4e451b14265109983a73d6c8
Guilherme Barbosa <guilherme.barbosa@tatic.net>
2022-11-02 18:09:45 -0300

    feat: strategy design pattern

---
+++ src/main/java/io/gdfbarbosa/patterns/behavioral/strategy/PayStrategy.java
@@ -0,0 +1,9 @@
+package io.gdfbarbosa.patterns.behavioral.strategy;
+
+/**
+ * Common interface for all strategies.
+ */
+public interface PayStrategy {
+    boolean pay(int paymentAmount);
+    void collectPaymentDetails();
+}
